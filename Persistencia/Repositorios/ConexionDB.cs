4bad4104a4919c52e1bbc2e76930cc79a8b46c14
Leandro Calvetti <lcalvetti1@gmail.com>
2020-10-07 21:26:01 -0300

    jdjjdjdjd

--- Persistencia/Repositorios/ConexionDB.cs
+++ Persistencia/Repositorios/ConexionDB.cs
@@ -9,13 +9,14 @@ namespace Persistencia.Repositorios
     {    
         public static MySqlConnection GetConexion()
         {
-            string cadenaConexion = "Database=prueba; Data Source=localhost; Port=3306; User Id=usuario1; Password=usuario1";
+            string cadenaConexion = "Database=base; Data Source=localhost; Port=3306; User Id=newuser; Password=newuser";
             MySqlConnection conexion = null;
             try
             {
                 conexion = new MySqlConnection(cadenaConexion);
                 Console.WriteLine("something is ");
             }catch (MySqlException ex){
+                string mensaje = ex.ToString();
                 Console.WriteLine("something is "+ex.ToString());
             }
             finally
