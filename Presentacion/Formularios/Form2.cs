e1b8e67daa88bf94d863c9f8afea34be8516faef
Leandro Calvetti <lcalvetti1@gmail.com>
2020-10-09 02:09:32 -0300

    subuir

--- Presentacion/Formularios/Form2.cs
+++ Presentacion/Formularios/Form2.cs
@@ -14,9 +14,13 @@ namespace Presentacion.Formularios
 {
     public partial class Form2 : Form
     {
+        private int v = 0;
         private int y = 70;      
         const int altura = 50;
+        //lista sintomas de la enfermedad
         List<DataSintoma> list;
+        //lista de todos los sintomas.
+        List<DataSintoma> listasintomas;
         private int indice = -1;
         public Form2()
         {
@@ -26,58 +30,100 @@ namespace Presentacion.Formularios
 
         private void Initcombox()
         {
+            
+            DataSintoma ds1 = new DataSintoma(1, "tos", 40.0);            
+            DataSintoma ds2 = new DataSintoma(2, "gripe", 30.0);                       
+            DataSintoma ds3 = new DataSintoma(3, "malestar", 20.0);
+            this.listasintomas = new List<DataSintoma>();            
+            this.listasintomas.Add(ds1);
+            this.listasintomas.Add(ds2);
+            this.listasintomas.Add(ds3);
             this.list = new List<DataSintoma>();
-
-            DataSintoma ds1 = new DataSintoma(1, "tos", 50.0);
-            this.list.Add(ds1);
-            DataSintoma ds2 = new DataSintoma(2, "gripe", 50.0);           
-            this.list.Add(ds2);
-
-            cmbsintomas.DataSource = this.list;
+            this.list.Add(ds1);            
+            this.list.Add(ds2);            
+            cmbsintomas.DataSource = this.listasintomas;
             cmbsintomas.ValueMember = "id";
             cmbsintomas.DisplayMember = "sintoma";
             //listo los sintomas;
+            ListarSintomas();
+            
+        }
+
+        //eliminar por nombre
+        private void EliminarSintomas()
+        {
+            foreach (Control item in Controls)
+            {
+                List<string> laux = this.listasintomas.Select(o=> o.Id.ToString()).ToList();
+                if (laux.Contains(item.Name))
+                {
+                    Controls.Remove(item);
+                    item.Dispose();
+                }
+                List<string> laux2 = this.listasintomas.Select(o => o.Sintoma.ToString()).ToList();
+                if (laux2.Contains(item.Name))
+                {
+                    Controls.Remove(item);
+                    item.Dispose();
+                }
+                List<string> laux3 = this.listasintomas.Select(o => o.Coef.ToString()).ToList();
+                if (laux3.Contains(item.Name))
+                {
+                    Controls.Remove(item);
+                    item.Dispose();
+                }
+
+            }
+        }
+
+        private void ListarSintomas()
+        {
             foreach (var item in this.list)
             {
 
                 Label lbl1 = new Label();
                 lbl1.Width = 46;
                 lbl1.Height = 17;
-                lbl1.Location = new Point(40,y);
+                lbl1.Location = new Point(40, y);
                 lbl1.Text = item.Id.ToString();
                 lbl1.Name = item.Id.ToString();
+                lbl1.Tag = this.v;
                 Label lbl2 = new Label();
                 lbl2.Width = 46;
                 lbl2.Height = 17;
                 lbl2.Location = new Point(143, y);
                 lbl2.Text = item.Sintoma.ToString();
                 lbl2.Name = item.Sintoma.ToString();
+                lbl2.Tag = this.v;
                 TextBox txt1 = new TextBox();
                 txt1.Width = 100;
                 txt1.Height = 22;
                 txt1.Location = new Point(300, y);
                 txt1.Text = item.Coef.ToString();
                 txt1.Name = item.Coef.ToString();
+                txt1.Tag = this.v;
                 txt1.TextChanged += new EventHandler((sender1, e1) => Txt_TextChanged(sender1, e1, txt1.Text, item.Id));
+                
                 //txt.TextChanged += Txt_TextChanged;
-                
-                
                 this.Controls.Add(lbl1);
                 this.Controls.Add(lbl2);
                 this.Controls.Add(txt1);
-
-
                 //cuadro de ponderacion
                 //aumentar la y
                 this.y += altura;
+                this.v++;
             }
         }
+
+        
+
+
 
         private void Txt_TextChanged(object sender, EventArgs e, string coef, long id)
         {
             //            
             //this.list.Find(x => x.Id == id);
-           bool encontrado = false;
+            bool encontrado = false;
             int cantidad = this.list.Count;
             int iter = 0;
             while (!encontrado)
@@ -125,5 +171,9 @@ namespace Presentacion.Formularios
 
         }
 
+        private void btnelim_Click(object sender, EventArgs e)
+        {
+            EliminarSintomas();
+        }
     }
 }
